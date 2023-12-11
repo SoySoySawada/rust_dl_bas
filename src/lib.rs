4c22963e8c756af7c383c99215192b06b7aa1212
S.Ichiki <95273775+SoySoySawada@users.noreply.github.com>
2023-12-11 17:57:40 +0900

    勾配逆伝播実装

--- src/lib.rs
+++ src/lib.rs
@@ -22,10 +22,10 @@ pub enum LayerType {
 }
 
 pub struct Layer {
-    weight_matrix: Array2<f64>,
+    pub weight_matrix: Array2<f64>,
     bias_array: Array1<f64>,
-    output_array: Array2<f64>,
-    du: Array2<f64>,
+    pub output_array: Array2<f64>,
+    pub du: Array2<f64>,
     layer_type: LayerType,
     next_layer: Option<Rc<Layer>>,
     prev_layer: Option<Rc<Layer>>,
@@ -41,6 +41,46 @@ impl Layer {
             layer_type,
             next_layer: None,
             prev_layer: None,
+        }
+    }
+
+
+
+    /// 活性化関数：ソフトマックス関数 として実装
+    pub fn calc_du_output_layer(&mut self, t: &Array2<f64>) {
+        match self.layer_type {
+            LayerType::Output => {
+                if t.shape() != self.output_array.shape() {
+                    panic!("t.shape() != self.output_array.shape()");
+                }
+                // dE/du を解析的に計算
+                self.du = (-t) * (Array2::from_elem(self.output_array.dim(), 1.0) - &self.output_array);
+            },
+            _ => {
+                panic!("calc_du_output_layer() is only for output layer");
+            }
+        }
+    }
+
+    /// 活性化関数：シグノイド関数 として実装
+    pub fn calc_du_hidden_layer(&mut self) {
+        match self.layer_type {
+            LayerType::Hidden => {
+                // dE/du を解析的に計算
+
+
+                let next_layer_du = self.next_layer.as_ref().unwrap().du.clone();
+                let next_layer_weight_matrix = self.next_layer.as_ref().unwrap().weight_matrix.clone();
+                let next_layer_weight_matrix = next_layer_weight_matrix.t();
+
+                let dy_du = &self.output_array * (Array2::from_elem(self.output_array.dim(), 1.0) - &self.output_array);
+                let de_dy = next_layer_du.dot(&next_layer_weight_matrix);
+
+                self.du = dy_du * de_dy;
+            },
+            _ => {
+                panic!("calc_du_hidden_layer() is only for hidden layer");
+            }
         }
     }
 }
